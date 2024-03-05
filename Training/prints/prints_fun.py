e9e9e46dd277a1857ecc1371ae2e5c739bd5fd3e
Gennaromaria Crispino <149183369+xgenn99@users.noreply.github.com>
2024-03-05 13:19:33 +0100

    Print during training

--- Training/prints/prints_fun.py
+++ Training/prints/prints_fun.py
@@ -1,5 +1,4 @@
 def prints(real_conv, resolution, in_channels, batch_size, epochs, lr, gamma, alpha, w1, w2, score_threshold):
-    
     print(f"The backbone is complex valued: {not real_conv}")
     print(f"in_channels: {in_channels} | resolution: {resolution} | batch_size: {batch_size} | epochs: {epochs} | lr: {lr} ")
     print(f"loss gamma: {gamma}| loss alpha: {alpha}| weight preds: {w1} | weight off: {w2}")
